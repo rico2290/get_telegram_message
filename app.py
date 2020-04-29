8d25a65442c0b22f15f375d1855f404c159791b0
rico2290 <rjl220990@gmail.com>
2020-04-29 13:57:49 -0300

    write message to csv

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from bs4 import BeautifulSoup as bs
 import urllib
-import codecs, os
+import codecs, os, csv, errno
 
 current_path = os.getcwd()
-print(current_path)
+try:
+    if not os.mkdir(f'{current_path}/temp'):
+        os.mkdir('temp')
+except OSError as e:
+    if e.errno == errno.EEXIST:
+        pass
+        #print('folder not created')
+    else:
+        raise
 
 for file in os.listdir():
-    print(file)
-
-with open('messages.html') as file:
-    contents = file.read()
-ss = bs(contents, 'html.parser')
-#soup = bs(contents, 'lxml')
-div_from_text = ss.findAll('div', attrs={'class': 'text'})
-div_body = ss.findAll('div', attrs={'class': 'body'})
-#print(type(div_body))
-for div in div_from_text:
-    print(f'\ntamanho: {len(div.text)}',div.text)
+    if file[-4:] in ['html', 'HTML']:
+        #print(file[:-5])
+        with open(file) as html_file:
+            contents = html_file.read()
+        ss = bs(contents, 'html.parser')
+        #soup = bs(contents, 'lxml')
+        div_from_text = ss.findAll('div', attrs={'class': 'text'})
+        div_body = ss.findAll('div', attrs={'class': 'body'})
+        #print(type(div_body))
+        
+        list_message = []
+        for div in div_from_text:
+            list_message.append(div.text.replace('"','').strip())
+            #print(f'\ntamanho: {len(div.text)}',div.text)
+        with open(r'temp/{}.csv'.format(file[:-5]), 'w', newline='',) as csv_file:
+            writer_file = csv.writer(csv_file, delimiter=',', escapechar="'", quoting=csv.QUOTE_NONE)
+            #print(list_message)
+              
+            #writer_file.writerow(['message'])
+            writer_file.writerow([message+'\n' for message in list_message])
+     
+                
+            
+            
